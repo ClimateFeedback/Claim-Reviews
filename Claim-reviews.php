cc1ec14e6a1fdf732f31501f278ab20f519088d8
Manu <emvincent@nsup.org>
2017-01-29 07:45:20 -0800

    first commit
    Create template basic

---
+++ Claim-reviews.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Claim-Reviews
+Plugin URI: https://github.com/ClimateFeedback/Claim-Reviews
+Description: Create post types for sentence-level claim reviews.
+Version: 0.1
+Author: Emmanuel Vincent
+License: BSD
+License URI: https://www.freebsd.org/copyright/freebsd-license.html
+*/
+
+  require_once('lib/claimreviewposts.php');      // Claim reviews custom post type
+
+?>
---
+++ lib/claimreviewposts.php
@@ -0,0 +1,244 @@
+<?php
+function create_claimreviews() {
+    $labels = array(
+        'name' => 'Claim-Reviews',
+        'singular_name' => 'Claim-Review',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New Claim-Review',
+        'edit' => 'Edit',
+        'edit_item' => 'Edit Claim-Review',
+        'new_item' => 'New Claim-Review',
+        'view' => 'View',
+        'view_item' => 'View Claim-Review',
+        'search_items' => 'Search Claim-Reviews',
+        'not_found' => 'No Claim-Reviews found',
+        'not_found_in_trash' => 'No Claim-Reviews found in Trash'
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => 4,
+        'taxonomies' => array( 'category', 'post_tag' ),
+        'supports' => array( 'title', 'thumbnail', 'excerpt', 'editor', 'revisions', 'claimreviews_admin' )
+    );
+
+    register_post_type( 'claimreview', $args);
+}
+
+add_action( 'init', 'create_claimreviews' );
+
+
+function claimreviews_admin() {
+    add_meta_box( 'claim_meta_box',
+        'Claim Reviews Details',
+        'display_review_meta_box',
+        'claimreview', 'normal', 'high'
+    );
+}
+
+add_action( 'admin_init', 'claimreviews_admin' );
+
+
+function display_review_meta_box( $claim ) {
+
+    $claimshort = esc_html( get_post_meta( $claim->ID, 'claimshort', true ) );
+
+    $date = esc_html( get_post_meta( $claim->ID, 'date', true ) );
+
+    $outlet = esc_html( get_post_meta( $claim->ID, 'outlet', true ) );
+    
+    $author = esc_html( get_post_meta( $claim->ID, 'author', true ) );
+
+    $annotationsLink = esc_html( get_post_meta( $claim->ID, 'annotationsLink', true ) );
+
+    $verdict = esc_html( get_post_meta( $claim->ID, 'verdict', true ) );
+
+    $details = esc_html( get_post_meta( $claim->ID, 'details', true ) );
+
+    $takeaway = esc_html( get_post_meta( $claim->ID, 'takeaway', true ) );
+
+    ?>
+
+    <div class="inside">
+        <div class="form-group">
+            <p class="wpt-form-label wpt-form-textfield-label">Claim short</p>
+            <input style="width: 100%" class="claimreview-meta" type="text" name="claim_short" value="<?php echo $claimshort; ?>" />
+        </div>
+
+        <div class="form-group">
+            <p class="wpt-form-label wpt-form-textfield-label">Claim Date</p>
+            <input style="width: 100%" class="claimreview-meta" type="text" name="claim_date" value="<?php echo $date; ?>" />
+        </div>
+
+        <div class="form-group">
+            <p class="wpt-form-label wpt-form-textfield-label">Outlet / Publisher</p>
+            <input style="width: 100%" class="claimreview-meta" type="text" name="claim_outlet" value="<?php echo $outlet; ?>" />
+        </div>
+
+        <div class="form-group">
+            <p class="wpt-form-label wpt-form-textfield-label">Author</p>
+            <input style="width: 100%" class="claimreview-meta" type="text" name="claim_author" value="<?php echo $author; ?>" />
+        </div>
+        
+        <div class="form-group">
+            <p class="wpt-form-label wpt-form-textfield-label">Link to Annotations</p>
+            <input style="width: 100%" class="claimreview-meta" type="text" name="claim_annotations" value="<?php echo $annotationsLink; ?>" />
+        </div>
+
+        <div class="form-group">
+            <p class="wpt-form-label wpt-form-textfield-label">Claim verdict</p>
+            <input style="width: 100%" class="claimreview-meta" type="text" name="claim_verdict" value="<?php echo $verdict; ?>" />
+        </div>
+
+        <div class="form-group">
+            <p class="wpt-form-label wpt-form-textfield-label">Details</p>
+            <p><em>example: <b>Inadequate backing of claim:</b> The authors did not provide a mechanism or evidence to support their claim.</em></p>
+            <textarea style="width: 100%" class="claimreview-meta" rows="5" name="claim_details" id="claim_details"><?php echo $details; ?></textarea>
+        </div>
+
+        <div class="form-group">
+            <p class="wpt-form-label wpt-form-textfield-label">Takeaway</p>
+            <p><em>example:.</em></p>
+            <input style="width: 100%" class="claimreview-meta" type="text" name="claim_takeaway" value="<?php echo $takeaway; ?>" />
+        </div>
+    </div>
+
+<?php }
+
+add_action( 'save_post', 'add_review_fields', 10, 2 );
+
+
+function add_review_fields( $claim_id, $claim ) {
+    if ( $claim->post_type == 'claimreview' ) {
+        // Store data in post meta table if present in post data
+
+        if ( isset( $_POST['claim_short'] ) && $_POST['claim_short'] != '' ) {
+            update_post_meta( $claim_id, 'claimshort', $_POST['claim_short'] );
+        }
+
+        if ( isset( $_POST['claim_date'] ) && $_POST['claim_date'] != '' ) {
+            update_post_meta( $claim_id, 'date', $_POST['claim_date'] );
+        }
+
+        if ( isset( $_POST['claim_outlet'] ) && $_POST['claim_outlet'] != '' ) {
+            update_post_meta( $claim_id, 'outlet', $_POST['claim_outlet'] );
+        }
+
+        if ( isset( $_POST['claim_author'] ) && $_POST['claim_author'] != '' ) {
+            update_post_meta( $claim_id, 'author', $_POST['claim_author'] );
+        }
+        
+        if ( isset( $_POST['claim_annotations'] ) && $_POST['claim_annotations'] != '' ) {
+            update_post_meta( $claim_id, 'annotationsLink', $_POST['claim_annotations'] );
+        }
+
+        if ( isset( $_POST['claim_verdict'] ) && $_POST['claim_verdict'] != '' ) {
+            update_post_meta( $claim_id, 'verdict', $_POST['claim_verdict'] );
+        }
+
+        if ( isset( $_POST['claim_details'] ) && $_POST['claim_details'] != '' ) {
+            update_post_meta( $claim_id, 'details', $_POST['claim_details'] );
+        }
+
+        if ( isset( $_POST['claim_takeaway'] ) && $_POST['claim_takeaway'] != '' ) {
+            update_post_meta( $claim_id, 'takeaway', $_POST['claim_takeaway'] );
+        }
+    }
+}
+
+function claimreviewsLoop( $atts ) {
+    extract( shortcode_atts( array(
+        "category" => '',
+        'type' => 'claimreview',
+    ), $atts ) );
+        
+    //Extract ID from category name
+    $theCatId = get_term_by( 'slug', $category, 'category' );
+    $theCatId = $theCatId->term_id;
+    
+    $output = '';
+    $paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
+    $args = array(
+        // 'post_parent' => $parent,
+        'post_type' => $type,
+        'sort_column'   => 'menu_order',
+        'cat' => $theCatId,
+        'posts_per_page' => 10,
+        'paged' => $paged
+    );
+    $yo_quiery = new WP_Query( $args );
+
+    while ( $yo_quiery->have_posts() ) : $yo_quiery->the_post();
+        $output .= '<div class="row">
+            <div class="media-left hidden-xs">
+                <a class="postpic" href="'. get_permalink( get_the_ID() ) .'">
+                    '. types_render_field( "front-image", array( "width" => "275", "height" => "176", "proportional" => "true" ) ) .'
+                </a>
+            </div>
+            <div class="media-body">
+                <a class="strong" href="'. get_permalink( get_the_ID() ) .'">
+                    <h3 class="noborder">'. get_the_title() .'</h3>
+                </a>
+                <h4 class="noborder"><span style="font-weight:normal; font-size-adjust: 0.5;">Claimed in</span> '. get_post_meta( get_the_ID(), 'outlet', true ) .', <span style="font-weight:normal; font-size-adjust: 0.5;">by</span> '. get_post_meta( get_the_ID(), 'author', true ) .'</h4>
+                <p class="small">
+                    <span>'. get_post_meta( get_the_ID(), 'date', true ) .'</span>
+                </p>
+                <p><em>'. get_the_excerpt() .'</em></p>
+            </div>
+        </div>
+        <hr/>';
+    endwhile;
+
+    wp_reset_query();
+    return $output;
+}
+add_shortcode('claimreviews-loop', 'claimreviewsLoop');
+
+
+
+function claimreview_pagination( $atts ) {
+    extract( shortcode_atts( array(
+        'type' => 'claimreview',
+    ), $atts ) );
+    $paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
+    $args = array(
+        // 'post_parent' => $parent,
+        'post_type' => $type,
+        'sort_column'   => 'menu_order',
+        'posts_per_page' => 10,
+        'paged' => $paged
+    );
+    $yo_quiery = new WP_Query( $args );
+
+    $yo_quiery->query_vars['paged'] > 1 ? $current = $yo_quiery->query_vars['paged'] : $current = 1;
+
+    $pagination = array(
+        'base' => @add_query_arg('page','%#%'),
+        'format' => '',
+        'total' => $yo_quiery->max_num_pages,
+        'current' => $current,
+        'show_all' => true,
+        'type' => 'list',
+        'next_text' => '&raquo;',
+        'prev_text' => '&laquo;'
+    );
+
+    if( !empty($yo_quiery->query_vars['s']) )
+        $pagination['add_args'] = array( 's' => get_query_var( 's' ) );
+
+
+    wp_reset_query();
+    return paginate_links( $pagination );
+}
+add_shortcode('paginate-claimreview', 'claimreview_pagination');
+
+?>
